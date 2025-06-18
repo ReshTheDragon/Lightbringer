c8ae491fc245b852f3c7c8027d0d3f67f07a30ed
DuongPKSE180524 <66048953+ReshTheDragon@users.noreply.github.com>
2025-06-18 08:39:57 +0700

    Revert "Merge remote-tracking branch 'origin/GroundLevel' into TestBranch"
    This reverts commit e9d77dd946c6a51b01b7975a43325308270f0e3a, reversing
    changes made to 8cc543293bb05fbe5c01112263f3b1924930ca8b.

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -3,14 +3,6 @@
 public class PlayerControl : MonoBehaviour
 {
     public float moveSpeed = 5f;
-    private Animator animator;
-    private SpriteRenderer spriteRenderer;
-
-    void Start()
-    {
-        animator = GetComponent<Animator>();
-        spriteRenderer = GetComponent<SpriteRenderer>();
-    }
 
     void Update()
     {
@@ -18,25 +10,17 @@ public class PlayerControl : MonoBehaviour
         float moveY = Input.GetAxisRaw("Vertical");
 
         Vector3 move = new Vector3(moveX, moveY, 0).normalized;
-        transform.position += move * moveSpeed * Time.deltaTime;
 
-        // Chuyển trạng thái Idle/Walk
-        bool isWalking = move.magnitude > 0;
-        animator.SetBool("IsWalking", isWalking);
+        transform.position += move * moveSpeed * Time.deltaTime;
 
         // Lật nhân vật theo hướng trái/phải
         if (moveX > 0)
         {
-            spriteRenderer.flipX = false;
+            GetComponent<SpriteRenderer>().flipX = false;
         }
         else if (moveX < 0)
         {
-            spriteRenderer.flipX = true;
-        }
-
-        if (Input.GetMouseButtonDown(0))
-        {
-            animator.SetTrigger("IsAttacking");
+            GetComponent<SpriteRenderer>().flipX = true;
         }
     }
 }
