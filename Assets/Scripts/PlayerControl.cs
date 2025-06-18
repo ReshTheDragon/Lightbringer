285b732ff145e524d4a4939b3c575ad15085eec0
DuongPKSE180524 <66048953+ReshTheDragon@users.noreply.github.com>
2025-06-18 09:00:23 +0700

    revert
    revert

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -3,6 +3,14 @@
 public class PlayerControl : MonoBehaviour
 {
     public float moveSpeed = 5f;
+    private Animator animator;
+    private SpriteRenderer spriteRenderer;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
 
     void Update()
     {
@@ -10,17 +18,25 @@ public class PlayerControl : MonoBehaviour
         float moveY = Input.GetAxisRaw("Vertical");
 
         Vector3 move = new Vector3(moveX, moveY, 0).normalized;
-
         transform.position += move * moveSpeed * Time.deltaTime;
+
+        // Chuyển trạng thái Idle/Walk
+        bool isWalking = move.magnitude > 0;
+        animator.SetBool("IsWalking", isWalking);
 
         // Lật nhân vật theo hướng trái/phải
         if (moveX > 0)
         {
-            GetComponent<SpriteRenderer>().flipX = false;
+            spriteRenderer.flipX = false;
         }
         else if (moveX < 0)
         {
-            GetComponent<SpriteRenderer>().flipX = true;
+            spriteRenderer.flipX = true;
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            animator.SetTrigger("IsAttacking");
         }
     }
 }
