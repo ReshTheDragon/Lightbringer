ee41f6c14e538998868c4325fcc6f8ea58394239
phong0210 <phong01062019@gmail.com>
2025-06-20 23:46:52 +0700

    monster animation

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -3,6 +3,11 @@
 public class PlayerControl : MonoBehaviour
 {
     public float moveSpeed = 5f;
+    public float attackRange = 1f;
+    public float knockbackForce = 5f;
+    public LayerMask enemyLayer;
+    public Transform attackPoint;
+
     private Animator animator;
     private SpriteRenderer spriteRenderer;
 
@@ -20,23 +25,37 @@ public class PlayerControl : MonoBehaviour
         Vector3 move = new Vector3(moveX, moveY, 0).normalized;
         transform.position += move * moveSpeed * Time.deltaTime;
 
-        // Chuyển trạng thái Idle/Walk
         bool isWalking = move.magnitude > 0;
         animator.SetBool("IsWalking", isWalking);
 
-        // Lật nhân vật theo hướng trái/phải
         if (moveX > 0)
-        {
             spriteRenderer.flipX = false;
-        }
         else if (moveX < 0)
-        {
             spriteRenderer.flipX = true;
-        }
 
         if (Input.GetMouseButtonDown(0))
         {
             animator.SetTrigger("IsAttacking");
+            Attack();
         }
+    }
+
+    void Attack()
+    {
+        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, enemyLayer);
+
+        foreach (Collider2D enemy in hitEnemies)
+        {
+            // Gọi hàm TakeHit trong enemy
+            enemy.GetComponent<EnemyFollow>().TakeHit(transform.position, knockbackForce);
+        }
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        if (attackPoint == null) return;
+
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(attackPoint.position, attackRange);
     }
 }
